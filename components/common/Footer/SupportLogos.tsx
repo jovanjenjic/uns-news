c38fce340829eb4d717b61bca5d6dac4e1909e85
Jovan Jenjic <jovan.jenjic@gmail.com>
2025-04-06 20:59:32 +0200

    Use createdDate field for articles

--- components/common/Footer/SupportLogos.tsx
+++ components/common/Footer/SupportLogos.tsx
@@ -4,7 +4,7 @@ const SupportLogos = () => {
   return (
     <div className="flex py-6 justify-center">
       <div className="flex flex-col justify-center">
-        <b className="mb-2">Пројекат суфинансиран od</b>
+        <b className="mb-2">Пројекат суфинансиран од</b>
         <div className="flex justify-center">
           <Image src="/img/MtoVertical.png" alt="MTO Logo" width={160} height={200} />
         </div>
