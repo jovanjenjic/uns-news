680e9d147dd7faf9bfc6c20ef7d9e58e087f3e02
Jovan Jenjic <jovan.jenjic@gmail.com>
2025-08-29 15:35:35 +0200

    Update Web app to fix build

--- next.config.js
+++ next.config.js
@@ -3,15 +3,6 @@ const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 const path = require('path')
 
 const withOffline = require('next-offline')
-// const nextOfflineConfig = {
-//   generateInDevMode: false,
-//   dontAutoRegisterSw: true,
-//   generateSw: false,
-//   workboxOpts: {
-//     swDest: 'static/service-worker.js',
-//     swSrc: path.join(__dirname, 'sw.js'),
-//   },
-// }
 
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
@@ -30,26 +21,9 @@ module.exports = (phase) => {
   }
 
   return withOffline({
-    generateInDevMode: false,
-    dontAutoRegisterSw: true,
-    generateSw: false,
     eslint: {
       ignoreDuringBuilds: true,
     },
-    workboxOpts: {
-      swDest: 'static/service-worker.js',
-      swSrc: path.join(__dirname, 'sw.js'),
-    },
-    // I'm using cloudinary as a media provider, but you can use any other provider
-
-    // This are the strapi docs of how to set a different provider
-    // https://strapi.io/documentation/v3.x/plugins/upload.html#using-a-provider
-    // And a list of the available providers
-    // https://www.npmjs.com/search?q=strapi-provider-upload-
-
-    // Also, this are the docs of to change the provider on next.js
-    // https://nextjs.org/docs/basic-features/image-optimization#configuration
-
     images: {
       domains: ['res.cloudinary.com'],
     },
